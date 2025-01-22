ca7cca326992c1613155ae16d6a23f7ae5a7ae2a
franky2003 <nkgd2003@gmail.com>
2025-01-22 16:43:34 +0530

    Initial Commit

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+import chefClaudeLogo from "./images/chef-claude-icon.png"
+
+export default function Header() {
+    return (
+        <header>
+            <img src={chefClaudeLogo}/>
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
+
+
+
+
+
+
+
+
+
